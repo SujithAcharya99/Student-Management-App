bdf923c296ed7376d53888f6aa0dffb8530c9eed
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-12-01 19:05:32 +0530

    Add files via upload

--- src/routers/studentroutes.js
+++ src/routers/studentroutes.js
@@ -8,11 +8,15 @@ const auth = require('../middleware/auth');
 const path = require('path');
 // const hbs = require('hbs');
 const bodyparser = require('body-parser');
+// const title } = require('process');
 const router = new express.Router();
 
+// app.use(express.json());
 router.use(bodyparser.urlencoded({
     extended: true
 }));
+
+// app.set('views', viewsPath);
 
 router.get('/', async (req, res) => {
     res.render('index',{
@@ -32,6 +36,16 @@ router.get('/test', async (req, res) => {
  
 })
 
+// app.get('/test2', async (req, res) => {
+//     res.render('test2',{
+//         title : 'Login Page',
+//         body : 'Sujith S'
+//     });
+ 
+// })
+
+
+
 router.get('/about', (req, res) => {
     res.render('about',{
         title : 'About Me',
@@ -39,8 +53,51 @@ router.get('/about', (req, res) => {
     });
 })
 
+// app.post('/test', async (req, res) => {
+
+//     const email = await req.body;
+
+//     console.log(email);
+//   res.send('success')
+// })
+
+//************************Login*****************88*8 */
+// router.get('/login', (req, res) => {
+
+//     res.render('loginandregister');
+// })
+
+
+
  router.post('/login', async (req, res) => {
 
+//     console.log('inside router', req);
+
+//     res.send(req.body);
+//     // try {
+    //     // const email = req.body.email;
+    //     // console.log(email)
+    //     const admin = await Admin.findOne({email: req.body.email, password: req.body.password});
+    //     if (admin) {
+           
+    //         return res.send('welcome admin');
+    //     }
+        
+    //     const user = await User.findOne({email: req.body.email, password: req.body.password});
+    //     // const token = await user.generateAuToken();
+    //     if (!user) {
+    //         return  res.send({
+    //             error : 'worng credentials'
+    //         })
+    //     } else {
+    //         // res.send(user);
+    //         res.redirect('/users');
+
+    //     }
+        
+    // } catch (e) {
+    //     res.status(400).send();
+    // }
     try {
          console.log(req.body)
         const admin = await Admin.findOne({email: req.body.email, password: req.body.password});
@@ -50,8 +107,13 @@ router.get('/about', (req, res) => {
         } else {
             console.log('inside else')
             console.log(req.body)
-        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const user = await User.findByCredentials(req.body.email, req.body.password);;
         const token = await user.generateAuToken();
+        // const student = await Student.findByCredentials(req.body.email, req.body.password);
+        // console.log(student)
+        if (!user) {
+            return res.status(404).send();
+        }
         console.log(user.roll)
          if (user.roll === 'not assigned') {
            
@@ -62,11 +124,14 @@ router.get('/about', (req, res) => {
                 name: 'Sujith S'
              });
          } else if(user.roll === 'student'){
-            console.log('in side student login logic')
-            console.log(req.body)
+            // console.log('in side student login logic')
+            // console.log(req.body)
             const student = await Student.findByCredentials(req.body.email, req.body.password);
             const token = await student.generateAuToken();
-            console.log(student)
+            // console.log(student)
+            if (!student) {
+                return res.status(404).send();
+            }
 
             res.render('test2',{
                 title : 'student Page',
@@ -74,18 +139,34 @@ router.get('/about', (req, res) => {
                 
             });
          } else if (user.roll === 'teacher'){
-            console.log(req.body)
+            // console.log(req.body)
             const teacher = await Teacher.findByCredentials(req.body.email, req.body.password);
             const token = await teacher.generateAuToken();
-             console.log('ready to render...');
+            //  console.log('ready to render...');
             //  console.log(teacher);
              const student = await Student.find({});
+             if (!teacher) {
+                return res.status(404).send();
+            }
             res.render('teacherDashboard',{
                 title : 'Teacher Page',
                  body : teacher.name,
-                student          
+                 subject: teacher.subjects_taught,
+                student
+                // teacher
+                
             });
          }
+        //  console.log(user)
+        //  res.send({ user, token });
+    //     Us.find({}, {}, function(e, docs) {
+
+    //         res.render('user-list', {'userlist' : docs});
+
+    //   });
+      
+        //res.send(user.roll);
+        // res.redirect('/user')
         }
     } catch (e) {
         res.status(400).send(e);
@@ -93,10 +174,10 @@ router.get('/about', (req, res) => {
     
 });
 
-router.get('/teacher/dashboard', async (req, res) => {
-    console.log('hello')
-    res.send('hello')
-})
+// router.get('/teacher/dashboard', async (req, res) => {
+//     console.log('hello')
+//     res.send('hello')
+// })
 //************************signup****************** */
 
 router.post('/signup', (req,res) => {
@@ -138,6 +219,21 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 });
 
+//************************user not assigned Page********* */
+
+// app.get('/user', (req, res) => {
+//     // const name = new User.findOne({email: req.body.email})
+//     // console.log(res.send(req.user));
+//     res.render('/notAssigned',{
+//         title : 'Home Page',
+//         user: 'user',
+//         message : 'Pls wait for Admin to assigned'
+//      });
+
+// })
+
+
+
 router.get('/user/me', auth, async (req, res) => {
     console.log('inside /users')
     res.send(req.user);
@@ -151,7 +247,11 @@ router.get('/admin/dashboard', async (req, res) => {
     // const userTeacher = await User.find({roll: 'teacher'});
     const teacher = await Teacher.find({});
     const user = await User.find({});
- 
+    // console.log( userTeacher.name,  userTeacher.email,  userTeacher.roll, userTeacher.age, userTeacher.password)
+
+//     const teacher = new Student(userTeacher)
+// console.log(userTeacher[0])
+
     res.render('adminDashboard',{
         admin: 'Admin',
         user,
@@ -205,6 +305,23 @@ router.get('/students', (req, res) => {
     })
 });
 
+router.get('/student/exam/:id', (req, res) => {
+    
+    // console.log(req.params.id)
+    const _id = req.params.id;
+
+    Student.findById({_id}).then((student) => {
+        // res.send(student);
+        res.render('Exam1', {
+            student
+        })
+    }).catch((e) => {
+        res.status(500).send();
+    })
+    // res.render('Exam')
+    
+});
+
 router.get('/students/:id', (req, res) => {
 
     const _id= req.params.id;
@@ -219,6 +336,7 @@ router.get('/students/:id', (req, res) => {
     })
 });
 
+
 router.get('/student/edit/:id', async (req, res) => {
     const _id = req.params.id;
     const student = await Student.findById(_id);
@@ -229,6 +347,8 @@ router.get('/student/edit/:id', async (req, res) => {
 
 })
 
+
+
 router.post('/student/edit/:id', async (req, res) => {
     console.log(req.body);
     const _id = req.params.id;
@@ -238,17 +358,26 @@ router.post('/student/edit/:id', async (req, res) => {
     const allowedUpdates = ['name', 'email', 'roll', 'age', 'subject'];
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
 
+
+
     if (!isValidUpdate) {
         return res.status(404).send({ error : 'Invalid Update...!'})
     }
 console.log('validation oki')
     try {
-     const student = await Student.findOneAndUpdate({ _id }, req.body, { new: true })     
+    //    const user = await User.findOneAndUpdate(email, req.body, { new : true, runValidators: true});
+     const student = await Student.findOneAndUpdate({ _id }, req.body, { new: true })
+    //    const user = await User.findById(_id);
+    // console.log(user)
+       
        updates.forEach((update) => student[update] = req.body[update])
+
     //    await req.user.save();
+
         if (!student) {
             return res.status(404).send();
         }
+
         res.redirect('/admin/dashboard');
 
     } catch (e) {
@@ -265,12 +394,16 @@ console.log('validation oki')
     
     const teacher = new Teacher(req.body)
     console.log(req.body)
+
     teacher.save().then(() =>{
+        // res.status(201).send(teacher)
         res.redirect('/admin/dashboard');
     }).catch((e) =>{
         res.status(400).send(e);
     })
 });
+
+
 
 router.get('/teachers', (req, res) => {
     Teacher.find({}).then((teacher) => {
@@ -286,9 +419,22 @@ router.get('/teacher/userEdit/:id', async (req, res) => {
     title : 'Teacher Page',
     user
 })
+    
 });
 
 router.get('/teacherUserList', async (req, res) => {
+    // const user = await User.find({roll:'not assigned'});
+
+    // if (!user) {
+    //     return res.student(400).send() 
+    // } else if (user.length === 0) {
+    //     return alert("no data found");
+    // }
+    //  else {
+    // res.render('teacherUserList', {
+    //     user,
+    //     name:'Sujith S'
+    // })}
     await User.find({roll:'not assigned'}).then((user) => {
         if (!user) {
             return res.student(400).send() 
@@ -306,21 +452,36 @@ router.get('/teacherUserList', async (req, res) => {
     })
 })
 router.post('/teacher/userEdit/:id', async (req, res) => {
+    // console.log(req.body);
     const _id = req.params.id;
+    // console.log(req.params.id)
     const updates = Object.keys(req.body);
+    // console.log(updates)
     const allowedUpdates = ['name', 'email', 'roll', 'age'];
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+
+
+
     if (!isValidUpdate) {
         return res.status(404).send({ error : 'Invalid Update...!'})
     }
+// console.log('validation oki')
     try {
-     const user = await User.findOneAndUpdate({ _id }, req.body, { new: true }) 
+    //    const user = await User.findOneAndUpdate(email, req.body, { new : true, runValidators: true});
+     const user = await User.findOneAndUpdate({ _id }, req.body, { new: true })
+    //    const user = await User.findById(_id);
+    // console.log(user)
+       
        updates.forEach((update) => user[update] = req.body[update])
+
     //    await req.user.save();
+
         if (!user) {
             return res.status(404).send();
         }
+
         res.redirect('/teacherUserList');
+
     } catch (e) {
         res.status(400).send();
     }
@@ -333,7 +494,10 @@ router.get('/teacher/edit/:id', async (req, res) => {
     title : 'Teacher Page',
     teacher
 })
+
 })
+
+
 
 router.post('/teacher/edit/:id', async (req, res) => {
     console.log(req.body);
@@ -344,28 +508,40 @@ router.post('/teacher/edit/:id', async (req, res) => {
     const allowedUpdates = ['name', 'email', 'roll', 'age', 'subjects_taught'];
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
 
+
+
     if (!isValidUpdate) {
         return res.status(404).send({ error : 'Invalid Update...!'})
     }
 console.log('validation oki')
     try {
-     const teacher = await Teacher.findOneAndUpdate({ _id }, req.body, { new: true })    
+    //    const user = await User.findOneAndUpdate(email, req.body, { new : true, runValidators: true});
+     const teacher = await Teacher.findOneAndUpdate({ _id }, req.body, { new: true })
+    //    const user = await User.findById(_id);
+    // console.log(user)
+       
        updates.forEach((update) => teacher[update] = req.body[update])
+
     //    await req.user.save();
+
         if (!teacher) {
             return res.status(404).send();
         }
+
         res.redirect('/admin/dashboard');
+
     } catch (e) {
         res.status(400).send();
     }
-})
+});
+
 
 /**************************8User**************8 */
 
 router.get('/newUser', (req, res) => {
     res.render('newUser')
-})
+});
+
 
 router.post('/users', (req, res) => {
     
@@ -376,8 +552,11 @@ router.post('/users', (req, res) => {
         res.redirect('/admin/dashboard')
     }).catch((e) =>{
         res.status(400).send(e);
-    })
+    });
 });
+
+
+
 
 router.get('/user/delete/:id', async (req, res) => {
 
@@ -410,24 +589,88 @@ router.get('/user/edit/:id', async (req, res) => {
 
     res.render('editUser',{
         user
-    })
-})
+    });
+});
+
+
+// router.patch('/users/:id', async (req, res) => {
+
+    // router.patch('/users/me', auth, async (req, res) => {
 
 router.post('/user/edit/:id', async (req, res) => {
         console.log(req.body);
         const _id = req.params.id;
         console.log(req.body.roll);
+        // if (req.body.roll === 'student') {
+        //     const student = req.body;
+
+        //     const _id = req.params.id;
+        //     console.log(_id)
+        
+        //     try {
+        //         const user = await User.findById(_id);
+        //         console.log(user)
+        //         if (!user) {
+        //             return res.status(404).send();
+        //         }
+        
+        //         await user.remove();
+                
+        //     } catch (e) {
+        //         res.status(500).send();
+        //     }
+
+
+        //     res.render('newStudentAdmin',{
+        //         student
+        //     })
+            
+        // } else if (req.body.roll === 'teacher') {
+        //     const teacher = req.body;
+
+        //     const _id = req.params.id;
+        //     console.log(_id)
+        
+        //     try {
+        //         const user = await User.findById(_id);
+        //         console.log(user)
+        //         if (!user) {
+        //             return res.status(404).send();
+        //         }
+        
+        //         await user.remove();
+                
+        //     } catch (e) {
+        //         res.status(500).send();
+        //     }
+
+
+            // res.render('newTeacherAdmin',{
+            //     teacher
+        //     })
+        // }
+        
+        // else {
+
             const student = req.body;
             const teacher = req.body;
             const updates = Object.keys(req.body);
         // console.log(updates)
         const allowedUpdates = ['name', 'email', 'roll', 'age'];
         const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+    
+
+    
         if (!isValidUpdate) {
             return res.status(404).send({ error : 'Invalid Update...!'})
         }
+    // console.log('validation oki')
         try {
+        //    const user = await User.findOneAndUpdate(email, req.body, { new : true, runValidators: true});
          const user = await User.findOneAndUpdate({ _id }, req.body, { new: true })
+        //    const user = await User.findById(_id);
+        // console.log(user)
+           
            updates.forEach((update) => user[update] = req.body[update])
     
         //    await req.user.save();
@@ -456,6 +699,14 @@ router.post('/user/edit/:id', async (req, res) => {
         
     })
 
+    // app.get('/users', (req, res) => {   
+//     User.find({}).then((user) => {
+//         res.send(user);
+//     }).catch((e) => {
+//         res.status(500).send(e);
+//     })
+// });
+
 //**************************Course****************** */
 router.get('/course', async(req, res) =>{
     const course = await Course.find({}); 
@@ -476,16 +727,16 @@ router.get('/newCourse', (req, res) =>{
 });
 router.post('/course', (req, res) => {
 
-    console.log(req.body)
+    console.log(req.body);
     
-    const course = new Course(req.body)
+    const course = new Course(req.body);
 
     course.save().then(() =>{
         // res.status(201).send(user)
-        res.redirect('/course')
+        res.redirect('/course');
     }).catch((e) =>{
         res.status(400).send(e);
-    })
+    });
 });
 
 //***********************HELP******************** */
