43cad5b44f581610eaec6e560e730dbe5d107c8d
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-12-03 19:41:02 +0530

    Add files via upload

--- src/models/test.js
+++ src/models/test.js
@@ -23,20 +23,49 @@ const testSchema = new mongoose.Schema({
         default: 00
     },
     questions: [{
-        mcq_question: {
+        mcq_question: [{
             type: String,
             required: true
-        },
-        options: {
+        }],
+        options: [{
             type: String,
             required: true
-        }
+        }]
     }],
-    answer: {
+    answer: [{
         type: String,
         required: true
-    }
+    }]
 });
+
+testSchema.statics.mcqData = async (data,que) => {
+     const test = this;
+
+
+     const mcq = new Test({
+        studentName: data.studentName,
+        subject: data.subject,
+        teacherName: data.teacherName,
+        //  questions:que,
+        // answer: data.answer
+    });
+    //  console.log(que)
+     const ar = que[0].mcq_question;
+    // // const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    // const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+    // // // console.log(token)
+    ar.forEach(element => {
+        // test.questions = test.questions.concat({ element });
+        console.log(element.toString())
+         mcq.answer = mcq.answer.concat({element}) ;
+    });
+    
+    
+     await mcq.save();
+    // console.log(que)
+    return;
+}
 
 const Test = mongoose.model('Test', testSchema);
 
