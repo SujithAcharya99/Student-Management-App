dcd4dadf0f33d1a63f994e1c7bbdb32e8cf6d464
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-12-31 18:53:42 +0530

    Add files via upload

--- src/models/chat_database.js
+++ src/models/chat_database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const Room = require('./room')
+// const { db } = require('./admin');
+
+// // const Test = mongoose.model('Test', {
 const chatSchema = new mongoose.Schema({
   id: {
     type: String,
@@ -26,16 +30,39 @@ const chatSchema = new mongoose.Schema({
 
 
 const Chat = mongoose.model('Chat', chatSchema);
+
+// //module.exports = Test;
+
 const users = [];
-const addUser = ({ id, username, room }) => {
+
+//adduser, removeUser, Getuser, getusersinroom
+
+const addUser = async ({ id, username, room }) => {
+  //clear the data 
 
   username = username.trim().toLowerCase(),
     room = room.trim().toLowerCase()
+
+  //validate the data
   if (!username || !room) {
     return {
       error: 'username and room are required'
     }
   }
+
+  //check for existing user 
+  // const existingUser = users.find((user) => {
+  //   return user.room === room && user.username === username
+  // })
+
+  // //validate username
+  // if (existingUser) {
+  //   return {
+  //     error: 'Username is in use..!'
+  //   }
+  // }
+
+
   async () => {
     const exist = await Chat.find({ name: username });
     console.log(exist)
@@ -45,29 +72,63 @@ const addUser = ({ id, username, room }) => {
       }
     }
   }
+
+
+
   // store user 
   const user = { id, username, room }
   // console.log(user)
   users.push(user)
   // console.log(users)
+
   const user_data = new Chat(user);
-  user_data.save().then((data) => {
+  await user_data.save().then((data) => {
+    // console.log(user_data)
+
     // console.log('success');
+
+    // return { user }
   }).catch((e) => {
     console.log(e)
   });
+  // console.log(user)
+
   return { user }
 }
+
+// const removeUser = async (id) => {
+// const removeUser =  (id) => {
+//   // const data = await Chat.findOneAndRemove({ id });
+//   // console.log('data from chat',data);
+//   // await data.remove();
+//   const index = users.findIndex((user) => user.id === id)
+//   console.log(index);
+//   if (index !== -1) {
+//     console.log(users.splice(index, 1)[0]);
+//     // console.log(users)
+//     return users.splice(index, 1)[0];
+//   }
+// }
+
 
 const removeUser = async (id) => {
   const data = await Chat.findOneAndRemove({ id });
   const index = users.findIndex((user) => user.id === id)
   console.log(index);
   if (index !== -1) {
-    console.log(users.splice(index, 1)[0])
-    return users.splice(index, 1)[0];
+    console.log('before return ::', users.splice(index, 1)[0])
+    // return users.splice(index, 1)[0];
+    const removedChatData = users.splice(index, 1)[0];
+    // return {
+    //   username: 'sujith',
+    //   room: 'student'
+    // };
+    console.log(removedChatData);
+    return removedChatData;
   }
 }
+
+
 
 const getUser = (id) => {
   // console.log(users.find((user) => user.id === id))
@@ -76,12 +137,96 @@ const getUser = (id) => {
 
 const getusersInRoom = async (room) => {
   const users = await Chat.find({});
-  console.log('geting room data')
+
+  // const users = await Chat.find({}).then((data) => {
+  //   return data
+  // })
+
+  // console.log('geting room data')
+  // console.log(room)
+  // console.log(users)
+  // for (const i in users) {
+
+  //   console.log(users[i].username)
+
+
+  // }
+  // async () => {
+  //   console.log('inside empty function')
+  //   const users = await Chat.find({});
+  //   console.log(users)
+  //   // return users
+  //   }
+  // console.log(users)
+
   return users;
+
+  // var jsonString = JSON.stringify(users);
+  // console.log(jsonString)
+  // return jsonString
+  // return users.filter((user) => user.room === room)
 }
 
+
+// const getusersInRoom = async (room) => {
+//   // console.log(users.filter((user) => user.room === room))
+//   return users.filter((user) => user.room === room)
+// const users = await Chat.find({room});
+// console.log(users)
+// return users
+// }
+
+// addUser({
+//     id: 22,
+//     username: 'sujith',
+//     room: 'odepoi'
+// })
+
+// addUser({
+//     id: 23,
+//     username: 'sanjay',
+//     room: 'odepoi'
+// })
+// addUser({
+//     id: 12,
+//     username: 'sharath',
+//     room: 'ijes'
+// })
+// // console.log(users)
+
+// const res = addUser({
+//     id:22,
+//     username: 'sujith',
+//     room:'odepoi'
+// })
+
+// const removedUsers = removeUser(22);
+
+// console.log(removedUsers);
+
+// console.log(users)
+
+// const user = getUser(12)
+// console.log(user)
+
+// const userlist = getusersInRoom('iqjes')
+
+// console.log(userlist)
 const msg = [];
-const generateMessage = (username, text) => {
+
+
+const generateMessage = (id, username, text) => {
+  msg.push({
+    id,
+    username,
+    text,
+    createdAt: new Date().getTime()
+  });
+  // console.log(msg)
+  // console.log(msg.reverse());
+  // const message = await Room.findOneAndUpdate({mainUser: id},{message:msg})
+  const message =  Room.findOneAndUpdate({mainUser: id},{message:msg})
+ 
   return {
     username,
     text,
@@ -89,7 +234,14 @@ const generateMessage = (username, text) => {
   }
 }
 
-const generateLocationMessage = (username, url) => {
+const generateLocationMessage = (id, username, url) => {
+  msg.push({
+    id,
+    username,
+    url,
+    createdAt: new Date().getTime()
+  });
+  // console.log(msg)
   return {
     username,
     url,
