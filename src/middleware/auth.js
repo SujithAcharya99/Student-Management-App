40780b5b1fbcbd6fc609fbdf0da3b314917a6a16
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2021-01-06 11:14:07 +0530

    Add files via upload

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,22 +5,23 @@ const auth = async (req, res, next) => {
     // console.log('auth middelware');
     // next();
     try {
-        const token = req.header('Authorization').replace('Bearer ', '');
+        // const token = req.header('Authorization').replace('Bearer ', '');
 
         // console.log(token);
-        console.log('token found');
+        // console.log('token found');
+        const token= req.headers.cookie.replace('jwt=','')
         const decoded = jwt.verify(token,'thisismynewcourse')
-        console.log('decoding success');
+        // console.log('decoding success');
         // const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findOne({ _id: decoded._id , 'tokens.token': token });
-        console.log('user found');
+        // console.log('user found');
         if (!user) {
             throw new Error();
         }
         //  req.token = token;
         // console.log('response success');
         req.user = user;
-
+        req.token=token
         next();
     } catch (e) {
         res.status(401).send({ error: 'Please Authenticate...!'});
@@ -29,3 +30,31 @@ const auth = async (req, res, next) => {
 }
 
 module.exports = auth;
+
+
+// const jwt=require('jsonwebtoken')
+// const Employee= require('../models/employeeModel')
+
+// const auth = async (req,res,next) => {
+//     try{
+//        const token= req.headers.cookie.replace('jwt=','')
+//         const decoded=jwt.verify(token,'thisisanapp')
+//         const employee= await Employee.findOne({_id: decoded._id ,'tokens.token':token})
+//         if(!employee){
+//             throw new Error()
+//         }
+//         req.employee=employee
+//         req.token=token
+//         next()
+//     }catch(e){
+//         res.render('employee/404Page',{
+//             message:'Authentication Failed !',
+//             url:'/employee/login'
+//         })
+        
+//     }
+
+// }
+
+
+// module.exports= auth
